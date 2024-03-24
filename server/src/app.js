39f3bdd8ce6bc7763a82438cd9d33519b766d189
Prafull Singh <prafullsingh975@gmail.com>
2024-03-24 18:07:17 +0530

    adding cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 
 const server = express();
 
+server.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
 server.use(express.json({limit:"16kb"}));
 server.use(express.urlencoded({extended:true,limit:"16kb"}));
 server.use(express.static("public"));
